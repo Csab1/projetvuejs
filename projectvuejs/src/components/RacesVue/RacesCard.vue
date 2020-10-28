5b6975708799450cdda74ae9c96f7c8137d6af34
Cyril <cyril.saboureau@edu.devinci.fr>
2020-10-28 17:37:09 +0100

    first commit

---
+++ projectvuejs/src/components/RacesVue/RacesCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="m-2 p-2 rounded border border-black flex justify-between"
+  >
+    <h3 class="text-lg font-bold">{{ races.Name }}</h3>
+    <p>Caractéristique : {{ races.carac}}</p>
+    <p>Taille : {{ races.taille}}</p>
+    <p>Vitesse : {{ races.vitesse}}</p>
+    <p>Competence : {{ races.Competence}}</p>
+    <p>Langue : {{ races.langue}}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "races-card",
+  props: ["races"],
+  
+};
+</script>
---
+++ projectvuejs/src/components/RacesVue/RacesList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1 class="text-lg font-bold text-center">
+        Liste des  {{racesData.name}}
+    </h1>
+    <races-card
+    v-for="(race,index) in racesData.races"
+    :key="index"
+    :race="race"
+    >
+    </races-card>
+  </div>
+</template>
+
+<script>
+import RacesCard from './RacesCard'
+import racesData from '../../data/races.json'
+
+
+
+export default {
+name:"races-list",
+components:{
+RacesCard,
+
+
+},
+data(){
+return {
+racesData: racesData,
+
+}
+
+}
+}
+</script>
+
+<style>
+
+</style>
